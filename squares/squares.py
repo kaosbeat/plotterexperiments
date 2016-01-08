25ea67aa1823ebf375780c5a31130c123d313c94
kaosbeat <kasper.jordaens@gmail.com>
2016-01-08 15:41:41 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -19,7 +19,7 @@ from scipy import signal
 # 	plot the results using a combination shape
 # first wave
 
-rez = 2000
+rez = 200 #keep it below 300 unless you know what you do
 size = 120
 noise = size*1.5
 interval = 50
