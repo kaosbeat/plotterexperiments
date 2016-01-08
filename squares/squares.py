70afb620a93172cce00ca9db90f60c6d11ac84c1
kaosbeat <kasper.jordaens@gmail.com>
2016-01-08 18:38:22 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -98,7 +98,7 @@ def somewaves(waves, subwaves, wavperiod = [], wavtype = [], wavesize = [], wave
 			# for i in range(int(randwavesize)):
 				
 				l = shapes.line(p1, p2)
-				transforms.perpendicular_noise(l, noise)
+				# transforms.perpendicular_noise(l, noise)
 				g.append(l)
 				p1 = p2
 				p2 = getrandompoint(wav, wavtype[idx], wavesize[idx])
