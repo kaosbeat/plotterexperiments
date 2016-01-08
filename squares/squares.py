0ba8b7c97c7cbae949f699bfdca45f056770a0eb
kaosbeat <kasper.jordaens@gmail.com>
2016-01-08 18:43:09 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -111,7 +111,7 @@ def somewaves(waves, subwaves, wavperiod = [], wavtype = [], wavesize = [], wave
 			print "done"
 
 
-somewaves(False, True, [wav1, wav2, wav2, wav1], ["sin", "cos", "cos", "sin"], [5100, 1000, 6500, 1000], ["rect", "cross", "cross", "cross"])  
+somewaves(False, True, [wav1, wav2, wav2, wav1], ["sin", "saw", "cos", "sin", "cos"], [5100, 1000, 6500, 4000, 1000], ["rect", "cross", "cross", "rect", "cross"])  
 # somewaves(True,False,[wav3],["saw"],[3000],["cross"])
 
 io.view(g)
