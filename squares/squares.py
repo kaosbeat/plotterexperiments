ab513a28a56fa6bb211f363a5d37b59450a105a4
kaosbeat <kasper.jordaens@gmail.com>
2016-01-08 16:14:45 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -37,7 +37,7 @@ g = shapes.group([])
 def getrandompoint(wave, table, Vsize):	
 	global randpointer
 	# global randpointerjump
-	randpointer = randpointer + int(randpointerjump/3 - random.randint(0, randpointerjump))
+	randpointer = randpointer + int(randpointerjump/3*2 - random.randint(0, randpointerjump))
 	if randpointer < 0:
 		randpointer = 0
 	if randpointer >= rez:
