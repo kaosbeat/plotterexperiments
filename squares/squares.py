aa944e758825e19396ee07b1e71ff49b15893fe3
kaosbeat <kasper.jordaens@gmail.com>
2016-01-08 14:00:40 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -43,7 +43,6 @@ def getrandompoint(wave, table, Vsize):
 	if table == "cos":
 		return (randpointer*interval, np.cos(wave[randpointer])*Vsize)
 
-def setwave()
 
 def somewaves(waves,subwaves):
 	for i in range(0,rez):
