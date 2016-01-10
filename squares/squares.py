da1f5633bbd1cebfb4fdf36f989c8793fd22dd05
kaosbeat <kasper.jordaens@gmail.com>
2016-01-10 21:59:01 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -115,7 +115,7 @@ somewaves(False, True, [wav1, wav2, wav2, wav3, wav1], ["sin", "saw", "cos", "si
 plotter.select_pen(2)
 #somewaves(True, True, [wav3, wav1], ["cos", "saw", "saw", "sin", "sin"], [1500, 5100, 500, 2000, 6000], ["rect", "cross", "cross", "rect", "cross"])  
 plotter.select_pen(3)
-somewaves(False, True, [wav1, wav2, wav2, wav1, wav3], ["sin", "saw", "cos", "sin", "cos"], [5100, 1000, 6500, 4000, 1000], ["rect", "cross", "cross", "rect", "cross"])  
+somewaves(False, True, [wav1, wav3, wav1, wav2, wav2], ["sin", "sin", "sin", "cos", "saw"], [5100, 1000, 500, 3000, 5000], ["rect", "cross", "cross", "rect", "cross"])  
 # somewaves(True,False,[wav3],["saw"],[3000],["cross"])
 
 print g.width, g.height
