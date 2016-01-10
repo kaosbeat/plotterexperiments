2d131ae5146469a1ec384f7017e0887a8b0e2240
kaosbeat <kasper.jordaens@gmail.com>
2016-01-10 20:17:25 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -2,7 +2,7 @@ from chiplotle import *
 # from chiplotle.tools.plottertools import instantiate_virtual_plotter
 # plotter =  instantiate_virtual_plotter(type="DXY1300")
 # plotter.margins.hard.draw_outline()
-plotter = instantiate_plotters(type="DXY1300")[0]
+# plotter = instantiate_plotters( )[0]
 plotter.select_pen(1)
 b = 0
 
