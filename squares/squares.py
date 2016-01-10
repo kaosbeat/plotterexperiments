e8727dee947a593fa252f4428dc115f00ca79146
kaosbeat <kasper.jordaens@gmail.com>
2016-01-10 22:08:21 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -120,7 +120,7 @@ noise = 200
 plotter.select_pen(2)
 #somewaves(True, True, [wav3, wav1], ["cos", "saw", "saw", "sin", "sin"], [1500, 5100, 500, 2000, 6000], ["rect", "cross", "cross", "rect", "cross"])  
 plotter.select_pen(3)
-somewaves(False, True, [wav1, wav3, wav1, wav2, wav2], ["sin", "sin", "sin", "cos", "saw"], [100, 1000, 3500, 3000, 5000], ["rect", "cross", "cross", "rect", "cross"])  
+somewaves(False, True, [wav1, wav3, wav1, wav2, wav2], ["sin", "sin", "sin", "cos", "saw"], [1000, 1500, 3500, 3000, 5000], ["rect", "cross", "cross", "rect", "cross"])  
 # somewaves(True,False,[wav3],["saw"],[3000],["cross"])
 print "second pass"
 print g.width, g.height
