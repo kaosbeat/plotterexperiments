6986019d9dd56152668480dd2c89183a82a15b4e
kaosbeat <kasper.jordaens@gmail.com>
2016-01-13 13:46:20 +0100

    Auto-committing 'squares2.py'

--- squares/squares2.py
+++ squares/squares2.py
@@ -88,7 +88,7 @@ def wavesdown(pen,freq, offset, min, max):
 	offset = math.randint(10, offset)
 	print offset
 	height = math.randint(min, max)
-	for f in range(0,freq)
+	for f in range(30)
 		#set height
 		p1 = (f + offset, height)
 		p2 = (f + offset, math.randint(0, max-height))
