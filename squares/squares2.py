f9fa3abb8b962b420cc97943c77e44b9cf3efda5
kaosbeat <kasper.jordaens@gmail.com>
2016-01-13 13:33:58 +0100

    Auto-committing 'squares2.py'

--- squares/squares2.py
+++ squares/squares2.py
@@ -75,7 +75,6 @@ def somewaves(pen,waves, subwaves, wavperiod = [], wavtype = [], wavesize = [], 
 				g.append(l)
 				p1 = p2
 				p2 = getrandompoint(wav, wavtype[w], wavesize[w])
-
 	
 io.view(g)
 
