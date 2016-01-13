50f333580fa007b6cbb93e81a9d768953be99305
kaosbeat <kasper.jordaens@gmail.com>
2016-01-13 14:18:07 +0100

    Auto-committing 'squares2.py'

--- squares/squares2.py
+++ squares/squares2.py
@@ -101,7 +101,7 @@ def wavesdown(pen,freq, offset, width, min, max):
 for i in range(10):
 	width = 50
 	freq=10
-	wavesdown(i,freq,i*freq*width,width,random.randint(500,1903),random.randint(2300,3000))
+	wavesdown(i,freq,i*width,width,random.randint(500,1903),random.randint(2300,3000))
 
 
 
