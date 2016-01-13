3ee71d3713d302d4cf5db992c3e58e66849a6a1e
kaosbeat <kasper.jordaens@gmail.com>
2016-01-13 09:30:00 +0100

    Auto-committing 'squares.py'

--- squares/squares.py
+++ squares/squares.py
@@ -112,7 +112,7 @@ def somewaves(waves, subwaves, wavperiod = [], wavtype = [], wavesize = [], wave
 					p1 = p2
 					p2 = getrandompoint(wav, wavtype[w], wavesize[w])
 				
-			print( g)
+			print(g.width)
 			plotter.write(g)
 
 
