952940ef8fd397a5ea969649e0b69e1221f361cc
kaosbeat <kasper.jordaens@gmail.com>
2016-01-13 13:37:27 +0100

    Auto-committing 'squares2.py'

--- squares/squares2.py
+++ squares/squares2.py
@@ -80,6 +80,14 @@ def somewaves(pen,waves, subwaves, wavperiod = [], wavtype = [], wavesize = [], 
 
 #somewaves(2,False, True, [wav3, wav1], ["cos", "saw", "saw", "sin", "sin"], [1500, 5100, 500, 2000, 6000], ["rect", "cross", "cross", "rect", "cross"])  
 
+def wavesdown(pen,freq):
+	global plotter
+	global g
+	plotter.select_pen(pen)
+	for f in range(freq)
+		
+
+
 
 
 
