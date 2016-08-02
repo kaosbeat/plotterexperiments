5d2121237cbe6aec64cd0e766016a06fd1d44562
kaosbeat <kasper.jordaens@gmail.com>
2016-08-02 20:57:49 +0200

    Auto-committing 'squares2.py'

--- squares2.py
+++ squares2.py
@@ -520,6 +520,7 @@ writeword("A3", 10, "USSR.ttf", 12500, 2900)
 writeword("automatic3CC", 10, "USSR.ttf", 17500, 2900)
 
 rhythmboxes(2,[1/2, 1/4],5000,2000,13500,0)
+
 rhythmboxes(1,[1/4, 1/4, 1/4],5000,2000,13500,4000)
 
 # print getspace((0,37),(300,84), 5)
