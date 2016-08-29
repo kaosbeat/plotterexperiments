d78bc45dce0a6916f9ed3c5d4f4efb9ade28185c
kaosbeat <kasper.jordaens@gmail.com>
2016-08-29 16:44:58 +0200

    Auto-committing 'hoes.py'

--- hoes.py
+++ hoes.py
@@ -28,7 +28,7 @@ import freetype
 # 	plot the results using a combination shape
 # first wave
 
-rez = 200 #keep it below 300 unless you know what you do
+rez = 20 #keep it below 300 unless you know what you do
 size = 10
 noise = size*1.5
 interval = 120
