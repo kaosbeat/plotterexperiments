f1ca1f26127aa3cf5ee8d0de457e32b05919008c
kaosbeat <kasper.jordaens@gmail.com>
2016-09-09 16:45:24 +0200

    Auto-committing 'squares2.py'

--- squares2.py
+++ squares2.py
@@ -583,7 +583,10 @@ startnumber = input('enter startnumber: ')
 stopnumber = input('enter stopnumber: ')
 
 for x in xrange(startnumber,stopnumber):
+	ready = input('is record ready? (y/n) :')
+	
 	plotcover(x, stopnumber)
+
 
 
 # print getspace((0,37),(300,84), 5)
