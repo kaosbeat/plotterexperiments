0eff741c838abecd884498cd187fd4b03494c1bc
kaosbeat <kasper.jordaens@gmail.com>
2016-09-09 14:40:06 +0200

    Auto-committing 'squares2.py'

--- squares2.py
+++ squares2.py
@@ -11,7 +11,8 @@ plotter =  instantiate_virtual_plotter(type="DXY1300")
 pltmax = [16158, 11040]
 plotlimits = [-17300,-11880,16340, 11880]  #dagos A1-plotter says (left: -17300, bottom: -11880, right: 16340, top: 11880) 84lmrn X 594mm
 plotleftbottom = [-17300,-11880]  # 1 coordinate unit per plotter = 0.025 mm
-plotsize = 
+plotsize = [626,309] #in mm breedte x hoogte
+
 #coords = plotter.margins.soft.all_coordinates
 # plotter.select_pen(1)
 b = 0
