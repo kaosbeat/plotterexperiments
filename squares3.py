4c0193d1b624496e977f78468bd053d2767090a5
kaosbeat <kasper.jordaens@gmail.com>
2016-11-12 16:40:28 +0100

    Auto-committing 'squares3.py'

--- squares3.py
+++ squares3.py
@@ -535,7 +535,9 @@ def plotcover(start, end):
 	marginsfront = [10,10,10,10] #mm [top,bottom,left,right]
 	frontoffset = (0,-plotsize[1]/2) #bottomleft x,y for front drawing
 	#gentopbounds
-	gentopXbounds = (marginsfront[2]/plotunit, plotsize[0]/2 - marginsfront[3]/plotunit) #(marginsfront[2]/plotunit, marginsfront[1]/plotunit) = (400,400)
+	# gentopXbounds = (marginsfront[2]/plotunit, plotsize[0]/2 - marginsfront[3]/plotunit) #(marginsfront[2]/plotunit, marginsfront[1]/plotunit) = (400,400)
+			
+	gentopXbounds = 
 	gentopYbounds = (marginsfront[0]/plotunit, plotsize[1] - marginsfront[1]/plotunit)
 	gentopXsize = gentopXbounds[1] - gentopXbounds[0]
 	gentopYsize = gentopYbounds[1] - gentopYbounds[0]
