42e721012280215b1a0aa781140fb91c7c71af0e
kaosbeat <kasper.jordaens@gmail.com>
2016-11-12 16:54:47 +0100

    Auto-committing 'squares3.py'

--- squares3.py
+++ squares3.py
@@ -552,12 +552,12 @@ def plotcover(start, end):
 	plotter.select_pen(4)
 	#hpgl.SP(4)
 	###def gentop(start,stop,min,max,steps,layer,xpos,ypos,offset=(0,0)): #(0,0),(10000,1000),(0,2000),(2500,5000),(3,2), 3
-	# gentop((gentopXbounds[0], 0.8*gentopYsize),(gentopXsize,0.2*gentopYsize),(0,0.3*gentopYsize),(0.4*gentopYsize,gentopYsize),(5,2),9, 0, 0, gentopoffset)
+	gentop((gentopXbounds[0], 0.8*gentopYsize),(gentopXsize,0.2*gentopYsize),(0,0.3*gentopYsize),(0.1*gentopYsize,0.5gentopYsize),(5,2),9, 0, 0, gentopoffset)
 	#plotter.clear()
 	# gentop((gentopXbounds[0], 0.8*gentopYsize),(0.3*gentopXsize,0.2*gentopYsize),(0,0.3*gentopYsize),(0.4*gentopYsize,gentopYsize),(5,2),9, 0, 0, gentopoffset)
 	
 	# gentop((0.3*gentopXsize,0.2*gentopYsize), (0.6*gentopXsize, 0.8*gentopYsize), (0,0.3*gentopYsize),(0.4*gentopYsize,gentopYsize),(5,2),9, 0, 0, gentopoffset)
-	gentop((0.6*gentopXsize, 0.8*gentopYsize), (gentopXsize,0.2*gentopYsize), (0,0.3*gentopYsize),(0.4*gentopYsize,gentopYsize),(5,2),9, 0, 0, gentopoffset)
+	# gentop((0.6*gentopXsize, 0.8*gentopYsize), (gentopXsize,0.2*gentopYsize), (0,0.3*gentopYsize),(0.4*gentopYsize,gentopYsize),(5,2),9, 0, 0, gentopoffset)
 
 	plotter.select_pen(1)
 	#hpgl.SP(1)
