3ed344f0760278e36fedecd479e9f64a8e34cc3c
kaosbeat <kasper.jordaens@gmail.com>
2016-11-12 17:24:50 +0100

    Auto-committing 'squares3.py'

--- squares3.py
+++ squares3.py
@@ -520,9 +520,9 @@ def plotcover(start, end):
 	###probeersel
 	plotter.select_pen(1)
 	#hpgl.SP(1)
-	# bounds = shapes.rectangle(plotsize[0], plotsize[1])
-	# transforms.offset(bounds,(-plotsize[0]/2 - 200, -plotsize[1]/2 - 200) )
-	# plotter.write(bounds)
+	bounds = shapes.rectangle(plotsize[0], plotsize[1])
+	transforms.offset(bounds,(-plotsize[0]/2 - 200, -plotsize[1]/2 - 200) )
+	plotter.write(bounds)
 
 	#zero = shapes.rectangle(100,10)
 	#plotter.write(zero)
