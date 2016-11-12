dde6b5cc153ca80dd01b49e2edd6d6082cb8e98b
kaosbeat <kasper.jordaens@gmail.com>
2016-11-12 17:32:07 +0100

    Auto-committing 'squares3.py'

--- squares3.py
+++ squares3.py
@@ -545,7 +545,7 @@ def plotcover(start, end):
 	# gentopXsize = 16340 + 17300
 	# gentopYsize = 11880 + 11880
 	# gentopoffset = (gentopXbounds[0], -plotsize[1]/2 + gentopYbounds[0]) #  + (plotsize[1]-gentopYsize)/2 )
-	gentopoffset = (-gentopXsize -1000,-gentopYsize)
+	gentopoffset = (-gentopXsize/2 -1000,-gentopYsize/2)
 	print(gentopXbounds,gentopYbounds, gentopYsize)
 	
 	#plotter.clear()
