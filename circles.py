6e9a093fce0e55a519e3874b7bbd333d676605c5
kaosbeat <kasper.jordaens@gmail.com>
2016-11-23 17:11:47 +0100

    Auto-committing 'circles.py'

---
+++ circles.py
@@ -0,0 +1,20 @@
+from __future__ import division
+from chiplotle import *
+
+from chiplotle.tools.plottertools import instantiate_virtual_plotter
+plotter =  instantiate_virtual_plotter(type="DXY1300")
+
+
+import math
+import random
+import numpy as np
+from scipy import signal
+
+
+
+def plot(start, end):
+	plotter.select_pen(1)
+	t = shapes.label(str(start) + "/" + str(end), 0.5, 0.5)
+	transforms.offset(t,(-2500, -plotsize[1]/2 + 200))
+	plotter.write(t)
+	io.view(plotter)
