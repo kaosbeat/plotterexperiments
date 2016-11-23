57f84e5941f25b66612ab0a92f488f051846c042
kaosbeat <kasper.jordaens@gmail.com>
2016-11-23 17:49:16 +0100

    Auto-committing 'circles.py'

--- circles.py
+++ circles.py
@@ -20,8 +20,8 @@ plotsize = [10, 10]
 
 def filledcircle(cx,cy,radius,rate):
 
-	a = np.linspace(0, 2*np.pi, rate)
-	# y = np.sin(x)
+	x = np.linspace(0, 2*np.pi, rate)
+	y = np.sin(x)
 	print (a)
 	# for i in xrange(1,100):
 	# 	print (y[i]*50)
