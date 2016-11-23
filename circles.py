439e0b66e0b1df92630946a9d4d68c8317e800b3
kaosbeat <kasper.jordaens@gmail.com>
2016-11-23 17:45:55 +0100

    Auto-committing 'circles.py'

--- circles.py
+++ circles.py
@@ -28,6 +28,7 @@ def filledcircle(cx,cy,radius,rate):
 		
 	g = shapes.group([])
 	for i in xrange(1,100):
+		print(a[i])
 		# g.append(shapes.line())
 		# g.append(shapes.line((x[i-1]*radius,y[i]*radius),(x[i]*radius, y[i]*radius)))
 		# g.append(shapes.line(
