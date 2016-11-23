41a3c325a8636c91f49b22a693bf6581dce9301e
kaosbeat <kasper.jordaens@gmail.com>
2016-11-23 17:13:59 +0100

    Auto-committing 'circles.py'

--- circles.py
+++ circles.py
@@ -13,11 +13,16 @@ from scipy import signal
 plotsize = [10, 10]
 
 
+def filledcircle(rate):
+	
+
+
 def plot(start, end):
 	plotter.select_pen(1)
 	t = shapes.label(str(start) + "/" + str(end), 0.5, 0.5)
 	transforms.offset(t,(-2500, -plotsize[1]/2 + 200))
 	plotter.write(t)
+
 	io.view(plotter)
 
 plot(0,1)
