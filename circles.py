690a02e80187289e31e2d4ad2f041ddb1d338be9
kaosbeat <kasper.jordaens@gmail.com>
2016-11-24 10:01:53 +0100

    Auto-committing 'circles.py'

--- circles.py
+++ circles.py
@@ -36,8 +36,9 @@ def filledcircle(cx,cy,radius,rate):
 			( cx + radius * np.cos(a[i]),cy + radius * np.sin(a[i]) )))
 		#figure out how to get point at certin y-value
 		# x^2 + y^2 = r^2
-		y = np.sqrt(radius^2 - x^2)
-
+		x0 = cx + radius * np.cos(a[i-1])
+		y0 = np.sqrt(radius^2 - x^2)
+		x1 = np.sqrt(radius^2 - y0^2)
 		# g.append(shapes.line(a,b))
 	plotter.write(g)
 
