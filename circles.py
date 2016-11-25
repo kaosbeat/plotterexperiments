a7a3031dd53704992775695c9073c7d0f602d08e
kaosbeat <kasper.jordaens@gmail.com>
2016-11-25 14:02:28 +0100

    Auto-committing 'circles.py'

--- circles.py
+++ circles.py
@@ -70,7 +70,9 @@ def plot(start, end):
 	transforms.offset(t,(-2500, -plotsize[1]/2 + 200))
 	plotter.write(t)
 	filledcircle(0,0,1000,50)
+	filledcircle(1000,500,1000,50)
 	takeawalk(80)
+
 	io.view(plotter)
 
 plot(0,1)
