31dc2a2fed6320439ea2f2d584e31ba32f3fd1de
kaosbeat <kasper.jordaens@gmail.com>
2016-11-27 20:22:51 +0100

    Auto-committing 'circles2.py'

--- circles2.py
+++ circles2.py
@@ -119,9 +119,9 @@ def takeawalk(size, rate):
 			
 			# else:
 				# path.append((x*100,x*1*(random.randint(0,x))))
-		g.append(shapes.bezier_path(path, 0 ,5))
-		plotter.write(g)
-		plotter.pen_up([(0,0)])
+			g.append(shapes.bezier_path(path, 0 ,5))
+			plotter.write(g)
+			plotter.pen_up([(0,0)])
 		
 		
 	
