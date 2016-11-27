99e549f4c29b7599da9767f9737eacfacc55c137
kaosbeat <kasper.jordaens@gmail.com>
2016-11-27 12:51:11 +0100

    Auto-committing 'circles2.py'

--- circles2.py
+++ circles2.py
@@ -118,7 +118,10 @@ def takeacirclewalk(steps):
 	plotter.write(g)
 
 def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
-	plotter.select_pen(1)
+	plotter.clear()
+	plotter.set_origin_center()
+
+	# plotter.select_pen(1)
 	plotter.write(shapes.rectangle(16158,11040))
 	# t = shapes.label(str(start) + "/" + str(end), 0.5, 0.5)
 	# transforms.offset(t,(-2500, -plotsize[1]/2 + 200))
