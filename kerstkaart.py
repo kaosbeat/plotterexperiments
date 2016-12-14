575e68524f26cef019e71087e3a81bd1da08a849
kaosbeat <kasper.jordaens@gmail.com>
2016-12-14 19:46:32 +0100

    Auto-committing 'kerstkaart.py'

--- kerstkaart.py
+++ kerstkaart.py
@@ -46,6 +46,7 @@ def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
 	offsetx = -2000
 	offsety = 0	
 	
+	
 
 	for x in xrange(1,10):
 		addobject1(random.randint(-8000,8000),random.randint(-5000,5000),random.randint(220,2000))
