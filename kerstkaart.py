9957e24d873a02182c3ee56197a413ae65778b56
kaosbeat <kasper.jordaens@gmail.com>
2016-12-14 21:24:34 +0100

    Auto-committing 'kerstkaart.py'

--- kerstkaart.py
+++ kerstkaart.py
@@ -60,7 +60,7 @@ def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
 	transforms.offset(t,(500, -200))
 	plotter.write(t)
 	plotter.select_pen(3)
-	kerstboom(1000,1000,1.4,15,80)
+	kerstboom(1000,1000,0.4,15,80)
 
 	for x in xrange(1,10):
 		addobject1(random.randint(-8000,8000),random.randint(-5000,5000),random.randint(220,2000))
