23da72ee86cc5fb60b3daf68c29b87b437706815
kaosbeat <kasper.jordaens@gmail.com>
2016-12-14 19:49:42 +0100

    Auto-committing 'kerstkaart.py'

--- kerstkaart.py
+++ kerstkaart.py
@@ -47,7 +47,7 @@ def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
 	offsety = 0	
 	plotter.select_pen(2)
 	#chiplotle.geometry.shapes.label(text, charwidth, charheight, charspace=None, linespace=None, origin='bottom-left')
-	t = shapes.label("Merel", 50, 50)
+	t = shapes.label("Merel", 5, 5)
 	# transforms.offset(t,(-2500, -plotsize[1]/2 + 200))
 	plotter.write(t)
 
