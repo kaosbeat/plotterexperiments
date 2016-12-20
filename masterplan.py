05c1087d4ffd7999a7bea88463d9528a18b3bf49
kaosbeat <kasper.jordaens@gmail.com>
2016-12-20 13:19:54 +0100

    Auto-committing 'masterplan.py'

--- masterplan.py
+++ masterplan.py
@@ -58,7 +58,7 @@ def kartelconnect(p1,p2,size):
 	sublength = (p2[1]-p1[1])/size
 	parts = int(abs(math.floor(length/sublength)))
 	partsspace = np.logspace(0.23 ,2, parts)
-	partsYspace = np.linspace(p1[0], p2[0], parts)
+	partsYspace = np.linspace(p1[0], p2[0], parts+1)
 	print(parts)
 	for i in xrange(0,parts):
 		print i
