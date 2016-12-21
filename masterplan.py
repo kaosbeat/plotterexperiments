ca45bbbb0620c419dcc0fd5cabd5912685eb4961
kaosbeat <kasper.jordaens@gmail.com>
2016-12-21 10:54:30 +0100

    Auto-committing 'masterplan.py'

--- masterplan.py
+++ masterplan.py
@@ -203,7 +203,7 @@ def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
 	# kartelconnect((1000,-5000), (3000,3000),50, False)
 	# kartelconnect((1000,5000), (300,3000),10, True)
 
-	for x in xrange(1,20):
+	for x in xrange(1,40):
 		addobject1(random.randint(-8000,6500),random.randint(-5000,3800),random.randint(220,2000))
 		# print(len(objects))
 	plotter.select_pen(1)
