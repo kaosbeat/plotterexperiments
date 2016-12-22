ae3f3d61ac2588cccb12805ecd756d66c576ecb9
kaosbeat <kasper.jordaens@gmail.com>
2016-12-22 20:54:01 +0100

    Auto-committing 'masterplan.py'

--- masterplan.py
+++ masterplan.py
@@ -212,7 +212,7 @@ def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
 	#  	# print(objects[x].get('x'))
 	#  	connectthedots(objects[x],objects[x+1])
 	g = shapes.group([])
-	s = 100
+	s = 1000
 	r = 10
 	for x in xrange(1,r):
 		g.append(shapes.line((0,-s*x*2),(-s*x,-r*s*2)))
