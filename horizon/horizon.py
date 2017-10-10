25aff22a42dd7819e00ff44773e621f051251089
kaosbeat <kasper.jordaens@gmail.com>
2017-10-10 18:11:50 +0200

    Auto-committing 'horizon.py'

--- horizon/horizon.py
+++ horizon/horizon.py
@@ -87,7 +87,7 @@ def renderline(data, y):
 	# //do perspective correction
 	for y in xrange(1,len(modulationdata)-1):
 		for x in xrange(1,len(inputdata)-1):
-			x1 = -x * 60 / y
+			x1 = -x * 600 / y
 			y1 = (60-c*y)*y+(y*50+(inputdata[x]*5 * modulationdata[y]))
 			x2 = -(x+1)*60/(y)
 			y2 = (60-c*y)*y+(y*50+inputdata[x+1]*5 * modulationdata[y])
