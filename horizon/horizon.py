742bc221d724ba9e5a3c8f7c8ad5000b229e69be
kaosbeat <kasper.jordaens@gmail.com>
2017-10-10 18:16:12 +0200

    Auto-committing 'horizon.py'

--- horizon/horizon.py
+++ horizon/horizon.py
@@ -96,7 +96,7 @@ def renderline(data, y):
 			y1 = (60-c*y)*y+(y*50+(inputdata[x]*5 * modulationdata[y]))
 			x2 = -x * 600 / (y+1)
 			y2 = (60-c*(y+1))* (y+1)+((y+1)*50+(inputdata[x]*5 * modulationdata[y+1]))
-			# g.append(shapes.line((x1,y1),(x2,y2)))
+			g.append(shapes.line((x1,y1),(x2,y2)))
 			# g.append(shapes)
 	plotter.write(g)
 
