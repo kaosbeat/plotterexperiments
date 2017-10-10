79e5233382b6c8cf1c544bc97e838d7a6f7c158b
kaosbeat <kasper.jordaens@gmail.com>
2017-10-10 17:39:49 +0200

    Auto-committing 'horizon.py'

--- horizon/horizon.py
+++ horizon/horizon.py
@@ -96,7 +96,7 @@ def renderline(data, y):
 
 for x in xrange(1,2):
 	for y in xrange(1,2):
-		plotPolygons(700,x*1800, -y*1800, x+3, y*100)
+		# plotPolygons(700,x*1800, -y*1800, x+3, y*100)
 		# plotCircles(1000, x*900, -y*1800, 50, 0)
 		# plotCircles(900, x*1800, -y*1800, 25, 0)
         # plotSquare(1000, x*1800, -y*1800, x*10, y*100)
