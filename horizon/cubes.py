b1071a65dda2dfc1b78cf56b2b9c14e93efc375e
kaosbeat <kasper.jordaens@gmail.com>
2017-12-05 16:55:33 +0100

    Auto-committing 'cubes.py'

--- horizon/cubes.py
+++ horizon/cubes.py
@@ -34,7 +34,7 @@ def plotCube(size, x, y):
 
 
 def plotDynamicCube(size, x, y, a1, a2, a3):  ## xf, yf, zf normalized vector
-    = cos(a1)
+    a1x = cos(a1)
     points = [(x,y),
               (x + (0.8*size), y - (0.2*size)),
               (x - (0.2*size), y + (0.8*size)),
