f8338de479117efac4e02d31ecb827608a0b72d8
kaosbeat <kasper.jordaens@gmail.com>
2017-12-05 19:02:11 +0100

    Auto-committing 'cubes.py'

--- horizon/cubes.py
+++ horizon/cubes.py
@@ -51,7 +51,7 @@ def plotDynamicCube(size, x, y, a1, a2, a3):  ## xf, yf, zf normalized vector
               (x + ((a1x+a2x)*size),y + ((a1y+a2y)*size))
     ]
     g = shapes.group([])
-    for i in range(100):
+    for i in range(7):
 
         g.append(shapes.line(points[i],points[i]))
     plotter.write(g)
