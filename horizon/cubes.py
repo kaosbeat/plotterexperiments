aa801e3f4ffec083dd0ad0c25b7e6d71234b364b
kaosbeat <kasper.jordaens@gmail.com>
2017-12-06 17:55:51 +0100

    Auto-committing 'cubes.py'

--- horizon/cubes.py
+++ horizon/cubes.py
@@ -106,7 +106,7 @@ def plotDynamicCube(size, x, y, a1, a2, a3, f1,f2,f3,f4):  ##a1,a2,a3 - slantvec
 
 
 
-plotDynamicCube(1000, 0, 0, -20, 20, 10,0,1,3,2)
-plotDynamicCube(1000, 0, 0, 0, 20, 10, 1,2,5,6)
+plotDynamicCube(600, 100, 0, -20, 20, 10,0,1,3,2)
+plotDynamicCube(1000, 300, -500, 0, 20, 10, 1,2,5,6)
 # plotCube(300, 500, 600)
 io.view(plotter)
