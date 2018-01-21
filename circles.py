41d826b07240b0fce7619c2a4515d3b95624af38
kaosbeat <kasper.jordaens@gmail.com>
2018-01-21 11:50:06 +0100

    Auto-committing 'circles.py'

--- circles.py
+++ circles.py
@@ -89,9 +89,9 @@ def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
 	# t = shapes.label(str(start) + "/" + str(end), 0.5, 0.5)
 	# transforms.offset(t,(-2500, -plotsize[1]/2 + 200))
 	# plotter.write(t)
-	# filledcircle(0,0,1000,50,0)
-	# filledcircle(1000,500,1000,50,1)
-	# filledcircle(1000,-500,1500,150,0)
+	filledcircle(0,0,1000,50,0)
+	filledcircle(1000,500,1000,50,1)
+	filledcircle(1000,-500,1500,150,0)
 	offsetx = -2000
 	offsety = 0
 	# for x in xrange(1,10):
@@ -99,8 +99,8 @@ def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
 	# 	plotter.select_pen((x % 4)+1)
 	# 	verticalfilledcircle(x*500 + offsetx, 0 + offsety, 2500+(x*250), 25,1 )
 
-	for x in xrange(1,20):
-		filledcircle(x*400, 2000, x*400, x*15,0 )
+	# for x in xrange(1,20):
+	# 	filledcircle(x*400, 2000, x*400, x*15,0 )
 	# takeawalk(80)
 
 	io.view(plotter)
