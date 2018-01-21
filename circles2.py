91b7df28de49b51858bbd25088b1281356fe1dd0
kaosbeat <kasper.jordaens@gmail.com>
2018-01-21 11:53:36 +0100

    Auto-committing 'circles2.py'

--- circles2.py
+++ circles2.py
@@ -147,7 +147,7 @@ def plot(start, end): #(left 0; bottom 0; right 16158; top 11040)
 	for x in xrange(1,10):
 		# print(x % 4)
 		plotter.select_pen((x % 4)+1)
-		# verticalfilledcircle(x*500 + offsetx, 0 + offsety, 3200+(x*250), 25,1)
+		verticalfilledcircle(x*500 + offsetx, 0 + offsety, 3200+(x*250), 25,1)
 		verticalfibcircle(x*500 + offsetx, 0 + offsety, 200+(x*250), 5,0)
 
 	# for x in xrange(1,12):
